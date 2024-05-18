a4e5293f249110dea8b001051a76e22bcecc817f
yaravaz <115376415+yaravaz@users.noreply.github.com>
2024-05-18 22:26:06 +0300

    Delete lab6 directory

--- lab6/lab6/src/by/poit/lab6/IConnection.java
+++
@@ -1,7 +0,0 @@
-package by.poit.lab6;
-
-public interface IConnection {
-    public Boolean getConnection();
-
-    public void closeConnection();
-}
--- lab6/lab6/src/by/poit/lab6/IQuery.java
+++
@@ -1,9 +0,0 @@
-package by.poit.lab6;
-
-import java.sql.ResultSet;
-
-public interface IQuery {
-    public ResultSet ExecuteQuery(String sqlQuery);
-    public ResultSet ExecutePrepareStatement(String language);
-    public void ExecuteTransaction();
-}
